161f0b140b1bfa666c559f8418fc7a0287f15189
Gawocko <carsonwgeorge@gmail.com>
2026-02-14 13:51:37 -0500

    Update Card.py

--- classes/Card.py
+++ classes/Card.py
@@ -11,13 +11,15 @@ class CardCondition:
     def __init__(self, tags: list[str] | None = None, cost_range: Tuple[int, int] | None = None):
         self.tags = tags or []
         self.cost_range = cost_range
-    
+
     def is_met(self, card) -> bool:
         if self.tags:
             if all(tag in card.tags for tag in self.tags):
                 return True
-        
-        if self.cost_range is not None and is_in_range(card.cost, self.cost_range[0], self.cost_range[1]):
+
+        if self.cost_range is not None and is_in_range(card.cost,
+                                                       self.cost_range[0],
+                                                       self.cost_range[1]):
             return True
-        
+
         return False
