168040b1309a06657b006ad9cc6b582b6a96337e
Gawocko <carsonwgeorge@gmail.com>
2026-02-14 13:54:57 -0500

    Update Card.py

--- classes/Card.py
+++ classes/Card.py
@@ -1,8 +1,11 @@
+"""Some classes to help with Card management."""
+
 from typing import Literal, Tuple
 from functions import *
 from classes.placeholders.placeholders import *
 
 class Card:
+    """Represents a card."""
     def __init__(self):
         self.type: CardCategory | None = None
         self.tags = []
